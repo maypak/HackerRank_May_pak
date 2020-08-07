c56ad908fa2d6b25a0c5dda8622ba664ba0ad71e
duvel666 <duvel66@gmail.com>
2020-08-07 23:40:38 +0900

    add SQL ~ 057

---
+++ SQL/057 Draw The Triangle 2/Solution.sql
@@ -0,0 +1,38 @@
+/*
+* With procedure
+*/
+
+DROP PROCEDURE IF EXISTS TRIANGLE;
+delimiter #
+CREATE PROCEDURE TRIANGLE()
+BEGIN
+DECLARE i INT DEFAULT 0;
+SET i=1;
+WHILE i<=20 DO
+    SELECT REPEAT('* ',i);
+    SET i=i+1;
+END WHILE;
+END#
+delimiter ;
+CALL TRIANGLE();
+
+/*
+* SELECT
+*/
+
+select
+    lpad('',n.n*2,'* ')
+from (
+    select
+        ones.n + tens.n*10 as n
+    from
+        (select 1 as n union select 2 union select 3 union
+		 select 4 union select 5 union select 6 union select 7 union
+		 select 8 union select 9 union select 10) ones
+    inner join
+        (select 0 as n union select 1) tens
+    on
+        true
+    order by
+        n asc
+) n;
