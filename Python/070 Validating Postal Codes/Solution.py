288a25395969dd629f3b97ae94e67bf397a27df6
duvel666 <duvel66@gmail.com>
2021-01-24 03:50:15 +0900

    add Python 070

---
+++ Python/070 Validating Postal Codes/Solution.py
@@ -0,0 +1,10 @@
+regex_integer_in_range = r"^[100000-999999]{6}$"	# Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(\d)(?=(\d)\1)"	# Do not delete 'r'.
+
+
+
+import re
+P = input()
+
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
