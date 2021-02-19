91c1b9c728e33db172b61165c5842f99c9f1f6a1
duvel666 <duvel66@gmail.com>
2021-02-19 12:24:43 +0900

    add Python 096

---
+++ Python/096 XML2 - Find the Maximum Depth/Solution.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as etree
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    for child in elem:
+        depth(child, level+1)
+    maxdepth = max(level+1, maxdepth)
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
