42eedce6d22872d15c5d000428390a5b622a65bb
duvel666 <duvel66@gmail.com>
2021-04-28 14:20:57 +0900

    add Algorithms 28

---
+++ Algorithms/28 Extra Long Factorials/Solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+        BigInteger result = new BigInteger("1");
+        for(int i = n; i > 1; i--){
+            result = result.multiply(new BigInteger(i+""));
+        }
+         System.out.println(result);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
